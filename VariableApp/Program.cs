2679c43d51336370f64a3bc52b436304d43c44b0
EldrichMaid <kupo@mail.ru>
2024-08-06 11:21:19 +0300

    Добавьте файлы проекта.

---
+++ VariableApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace VariableApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "NewName";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
