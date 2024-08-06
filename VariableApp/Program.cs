7ed78b63e6c7b4c73b33d7d7db10125e2dd97577
EldrichMaid <kupo@mail.ru>
2024-08-06 11:33:48 +0300

    Объявлена константа "ConstantName"

--- VariableApp/Program.cs
+++ VariableApp/Program.cs
@@ -7,6 +7,8 @@
             string MyName;
             MyName = "NewName";
             Console.WriteLine(MyName);
+            const string ConstantName = "Permanence";
+            Console.WriteLine(ConstantName);
             Console.ReadKey();
         }
     }
