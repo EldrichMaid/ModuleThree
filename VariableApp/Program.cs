cc43dd7a1e6ce517a079f117a69828d7692645f0
EldrichMaid <kupo@mail.ru>
2024-08-06 12:48:02 +0300

    Добавлена практика по типам данных в строках 17 - 25

--- VariableApp/Program.cs
+++ VariableApp/Program.cs
@@ -13,6 +13,16 @@
             Console.WriteLine("\t  You now know 'bout literals");
             Console.WriteLine("\t  Congrats to you,darlin'");
             Console.WriteLine("\x23");
+
+            string AdditionalName = "Ivan";
+            byte Age = 28;
+            bool PetData = false;
+            double ShoeSizeData = 41.5;
+
+            Console.WriteLine("My name is " + AdditionalName);
+            Console.WriteLine("MyAge " + Age);
+            Console.WriteLine("Do I have a pet? " + PetData);
+            Console.WriteLine("My shoe size is " + ShoeSizeData);
             Console.ReadKey();
         }
     }
