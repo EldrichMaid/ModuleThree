20bd6ad970e6856225b24569615b9d3b4ecb70ca
EldrichMaid <kupo@mail.ru>
2024-08-06 15:47:41 +0300

    Добавлена конвертация типов данных

--- VariableApp/Program.cs
+++ VariableApp/Program.cs
@@ -54,6 +54,13 @@
             double DivBottom = 2;
             double result = DivTop / DivBottom;
             Console.WriteLine("{0} / {1} = {2}", DivTop, DivBottom, result);
+
+            Console.Write("Enter your name: ");
+            string nameline = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte agerange = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1} ", nameline, agerange);
+            
             Console.ReadKey();
         }
         enum DaysOfWeek : byte
